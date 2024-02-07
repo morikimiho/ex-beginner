feedd5bd979b3bd8f37e52b58342500cabd14342
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2024-02-07 15:43:55 +0900

    曖昧検索可能

--- src/main/java/com/example/exbeginner/controller/Exam05Controller.java
+++ src/main/java/com/example/exbeginner/controller/Exam05Controller.java
@@ -1,11 +1,15 @@
 package com.example.exbeginner.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.exbeginner.model.Member;
 import com.example.exbeginner.service.MemberService;
 
 @Controller
@@ -20,8 +24,9 @@ public class Exam05Controller {
     }
 
     @PostMapping("/search")
-    public String search(String name) {
-        service.searchMember(name);
+    public String search(String name, Model model) {
+        List<Member> members = service.searchMember(name);
+        model.addAttribute("members", members);
         return "exam05-result";
     }
 
--- src/main/java/com/example/exbeginner/repository/MemberRepository.java
+++ src/main/java/com/example/exbeginner/repository/MemberRepository.java
@@ -1,23 +1,42 @@
 package com.example.exbeginner.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.exbeginner.model.Member;
 
 @Repository
 public class MemberRepository {
+    private static final String SEARCH_MEMBER ="""
+            SELECT
+                name
+            FROM
+                members
+            WHERE
+                name LIKE :name;
+            """;
+
+    private static final RowMapper<Member> MEMBER_ROWMAPPER = (rs, rowNum) -> {
+        Member member = new Member();
+        member.setName(rs.getString("name"));
+        return member;
+    };
+
     @Autowired
     private NamedParameterJdbcTemplate template;
 
     public List<Member> searchMember(String name) {
-        System.out.println("Repositoryが呼ばれた");
-        System.out.println(name);
-        return new ArrayList<Member>();
+        SqlParameterSource params = new MapSqlParameterSource()
+            .addValue("name", "%" + name + "%");
+        List<Member> members = template.query(SEARCH_MEMBER, params, MEMBER_ROWMAPPER);
+
+        return members;
     }
     
 }
